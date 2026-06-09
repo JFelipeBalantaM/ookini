82b9a917b651ea535b57d9b6351f03eeb927b869
Juan Felipe <fibala00@gmail.com>
2026-06-09 15:43:47 -0500

    Versión inicial proyecto Ookini

---
+++ script.js
@@ -0,0 +1,24 @@
+function calcularPrecio(precioUnitario, cantidad) {
+    const total = precioUnitario * cantidad
+    return total
+}
+
+function puedeReservar(tazasDisponibles) {
+    return tazasDisponibles > 0;
+}
+
+const botonReservar = document.querySelector("#boton-reservar");
+const contadorTazas = document.querySelector("#contador-tazas");
+
+botonReservar.addEventListener("click", function() {
+    const tazasActuales = Number(contadorTazas.textContent)
+
+    if (puedeReservar(tazasActuales)) {
+        contadorTazas.textContent = tazasActuales - 1
+        alert("Reserva registrada")
+    } else {
+        botonReservar.textContent = "Sin cupos"
+        botonReservar.disabled = true
+        alert("No hay cupos")
+    }
+})
